a539a0095c424a981d717d00fd2c2774ff297ea4
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2023-01-09 11:51:10 -0500

    completed all exercises

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Kassandra";
+let lastName;
+var age;
 // Exercise 2
-
+lastName = "Johnson";
+age = 34;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePerecntage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
